98f20fe355eaaf245bca49fae9811291dbe62008
Ladislav Čapka <lcapka@users.noreply.github.com>
2023-10-08 13:43:52 +0200

    refactor: all (#1)

---
+++ asset/main.js
@@ -0,0 +1,5 @@
+const contactEmail = atob("bWFpbHRvOmluZm9AY3licm9zbGFicy5jb20=");
+const emailLinks = document.querySelectorAll('[data-contact-email]');
+emailLinks.forEach(link => {
+    link.onmouseover = link.ontouchstart = () => link.setAttribute('href', contactEmail);
+});
