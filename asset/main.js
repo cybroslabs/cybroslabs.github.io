4ea5db87cfbd41132e8e7a91a2c7600d5151f032
Ladislav Čapka <lcapka@gmail.com>
2023-10-09 14:39:43 +0200

    refactor: various

--- asset/main.js
+++ asset/main.js
@@ -1,5 +1,9 @@
-const contactEmail = atob("bWFpbHRvOmluZm9AY3licm9zbGFicy5jb20=");
+const contactEmail = atob("aW5mb0BjeWJyb3NsYWJzLmNvbQ==");
 const emailLinks = document.querySelectorAll('[data-contact-email]');
 emailLinks.forEach(link => {
-    link.onmouseover = link.ontouchstart = () => link.setAttribute('href', contactEmail);
+    link.onmouseover = link.ontouchstart = () => link.setAttribute('href', 'mailto:'+contactEmail);
+});
+const emailTexts = document.querySelectorAll('[data-contact-email-print]');
+emailTexts.forEach(link => {
+    link.textContent = contactEmail;
 });
