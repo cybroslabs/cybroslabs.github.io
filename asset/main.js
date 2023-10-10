802db689a6ec57f290ad71e66f9d91bf01f38ba6
Ladislav Čapka <lcapka@gmail.com>
2023-10-10 22:13:34 +0200

    docs: add phone contact

--- asset/main.js
+++ asset/main.js
@@ -7,3 +7,13 @@ const emailTexts = document.querySelectorAll('[data-contact-email-print]');
 emailTexts.forEach(link => {
     link.textContent = contactEmail;
 });
+
+const contactPhone = atob("KzQyMCA3NzcgMzQ0IDQ4Nw==");
+const phoneLinks = document.querySelectorAll('[data-contact-phone]');
+phoneLinks.forEach(link => {
+    link.onmouseover = link.ontouchstart = () => link.setAttribute('href', 'tel:'+contactPhone.replace(/ /g, ''));
+});
+const phoneTexts = document.querySelectorAll('[data-contact-phone-print]');
+phoneTexts.forEach(link => {
+    link.textContent = contactPhone;
+});
